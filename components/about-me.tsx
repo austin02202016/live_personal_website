17b85cfca9a178c9632bed746aba5ee9c8d1fc75
Austin Kennedy <austin25@illinois.edu>
2024-12-19 20:18:19 -0600

    background of 'my life' changed to match that of other sections

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -6,12 +6,11 @@ import { motion } from "framer-motion";
 import { useInView } from 'react-intersection-observer';
 
 export function AboutMeSection() {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Change this to false if you want the animation to trigger again whenever it comes in view
+  });
 
-    const { ref, inView } = useInView({
-      triggerOnce: true, // Change this to false if you want the animation to trigger again whenever it comes in view
-    });
-    
-    const timelineEvents = [
+  const timelineEvents = [
     { year: 2003, event: 'Born in Elmwood Park to some great parents. Shoutout Mom and Dad' },
     { year: 2018, event: 'Graduated from St. Celestine Elementary, started playing baseball at Illinois Travel Baseball Club in Mount Prospect. Played pretty much every sport in 8th grade - baseball, basketball, hockey, soccer, track and field, cross-country, and bowling' },
     { year: 2019, event: 'Made the freshman basketball team, got my first job at Habetler Bowl in Jefferson Park, read "Can&#39;t Hurt Me" by David Goggins and got deep into the self-help space, got big into the gym and science-based lifting' },
@@ -20,56 +19,65 @@ export function AboutMeSection() {
     { year: 2022, event: 'Graduated high school at Notre Dame College Prep, lost in sectional final for high school baseball final and retired, varsity bowler captain, managed the frozen department at Mariano&#39;s in Harwood Heights' },
     { year: 2023, event: 'Went skydiving, worked a door-to-door sales job (very character building), started attending Luke Clancy&#39;s jam sessions and overdosed on the entrepreneurship pill' },
     { year: 2024, event: 'Interned at Innovo Markets and transitioned to a consultant role thereafter, ran a marathon, helped with the founding and growth of Illini Run Club, started recording short-form reels for Proof of Concept' },
-    ];
+  ];
 
-    return (
-      <motion.div className="min-h-screen bg-white text-black font-sans" ref={ref}>
-        <motion.div className="container mx-auto px-[10%] py-24">
-          <motion.h2 
-            className="text-4xl sm:text-5xl lg:text-6xl font-extrabold text-gray-900 text-center mb-24"
-            initial={{ opacity: 0, y: -20 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5 }}
-          >
-            Deep Dive About Me
-          </motion.h2>
-          <motion.p className="text-xl mb-12 text-center max-w-2xl mx-auto"
-            initial={{ opacity: 0, y: -20 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5 }}
-          >
-            Here&#39;s a detailed description of my life. Long story short - I&#39;ve been down a lot of different rabbit holes.
-          </motion.p>
+  return (
+    <motion.div
+      className="min-h-screen bg-gray-50 text-black font-sans"
+      ref={ref}
+    >
+      <motion.div className="container mx-auto px-4 sm:px-6 lg:px-8 py-24">
+        <motion.h2
+          className="text-4xl sm:text-5xl lg:text-6xl font-extrabold text-gray-900 text-center mb-16"
+          initial={{ opacity: 0, y: -20 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.5 }}
+        >
+          Deep Dive About Me
+        </motion.h2>
+        <motion.p
+          className="text-xl mb-12 text-center max-w-2xl mx-auto"
+          initial={{ opacity: 0, y: -20 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.5 }}
+        >
+          Here&#39;s a detailed description of my life. Long story short - I&#39;ve been down a lot of different rabbit holes.
+        </motion.p>
 
-          <motion.div className="flex justify-center mb-8"
-            initial={{ opacity: 0, y: -20 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5 }}
-          >
-            <ChevronDown className="animate-bounce w-8 h-8" />
-          </motion.div>
+        <motion.div
+          className="flex justify-center mb-8"
+          initial={{ opacity: 0, y: -20 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.5 }}
+        >
+          <ChevronDown className="animate-bounce w-8 h-8" />
+        </motion.div>
 
-          <motion.div className="border border-gray-200 rounded-lg shadow-lg p-8 bg-white"
-            initial={{ opacity: 0, y: -20 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5 }}
-          >
-            <div className="space-y-12">
-              {timelineEvents.map((item, index) => (
-                <div key={index} className="flex">
-                  <div className="flex flex-col items-center mr-4">
-                    <div className="w-px h-full bg-gray-300"></div>
-                    <div className="w-4 h-4 rounded-full bg-black mt-1"></div>
-                  </div>
-                  <div className="pb-8">
-                    <p className="text-2xl font-bold">{item.year}</p>
-                    <p className="text-gray-600" dangerouslySetInnerHTML={{ __html: item.event }}></p>
-                  </div>
+        <motion.div
+          className="border border-gray-200 rounded-lg shadow-lg p-8 bg-white"
+          initial={{ opacity: 0, y: -20 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="space-y-12">
+            {timelineEvents.map((item, index) => (
+              <div key={index} className="flex">
+                <div className="flex flex-col items-center mr-4">
+                  <div className="w-px h-full bg-gray-300"></div>
+                  <div className="w-4 h-4 rounded-full bg-black mt-1"></div>
                 </div>
-              ))}
-            </div>
-          </motion.div>
+                <div className="pb-8">
+                  <p className="text-2xl font-bold">{item.year}</p>
+                  <p
+                    className="text-gray-600"
+                    dangerouslySetInnerHTML={{ __html: item.event }}
+                  ></p>
+                </div>
+              </div>
+            ))}
+          </div>
         </motion.div>
       </motion.div>
-    )
+    </motion.div>
+  );
 }
